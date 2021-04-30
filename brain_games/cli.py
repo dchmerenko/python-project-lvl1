acb2cf6b5161896d89cd0f669acaf4a004b1cc85
Dmitry Chmerenko <dchmerenko@gmail.com>
2021-04-30 13:05:23 +0300

    Fix Flake8 issues

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,9 @@
+"""CLI functions module."""
+
 import prompt
 
 
 def welcome_user():
+    """Prompt User name and print welcome message."""
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!') 
-
+    print('Hello, {name}!'.format(name=name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
+"""Brain-games main module."""
 
 from brain_games.cli import welcome_user
 
 
 def main():
-    print("Welcome to the Brain Games!")
+    """Brain-game main function."""
+    print('Welcome to the Brain Games!')
     welcome_user()
 
 
 if __name__ == '__main__':
     main()
-
