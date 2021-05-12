6aea9f4161125f1552a1478fc850ba57011cab11
Dmitry Chmerenko <dchmerenko@gmail.com>
2021-05-13 02:13:32 +0300

    Refactor lib.py:base_play
    Join several functions to one

--- brain_games/out.py
+++ brain_games/out.py
@@ -4,73 +4,30 @@
 import prompt
 
 
-def welcome_user():
-    """
-    Prompt User name and print welcome message. Return User name.
-
-    Returns:
-         User name
-    """
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {name}!'.format(name=name))
-    return name
-
-
-def base_play(*, start_msg, tries_limit, generate_question, calculate_answer):
+def base_play(*, start_msg, tries_limit, get_question_answer):
     """Return Base play function.
 
     Args:
         start_msg: start game message
         tries_limit: tries limit
-        generate_question: question generating function
-        calculate_answer: answer calculating function
+        get_question_answer: question-answer generator
     """
-    user = welcome_user()
+    print('Welcome to the Brain Games!')
+    user = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(user))
+
     print(start_msg)
 
     for _ in range(tries_limit):
-        question, args = generate_question()
-        correct_answer = calculate_answer(args)
-        user_answer = get_user_answer(question)
-        if user_answer == correct_answer:
+        question, answer = get_question_answer()
+        print('Question: {0}'.format(question))
+        user_answer = prompt.string('Your answer: ', empty=True)
+        if user_answer == answer:
             print('Correct!')
             continue
-        process_wrong_answer(user_answer, correct_answer)
-        print("Let's try again, {user}!".format(user=user))
+        wrong_answer_msg = "'{0}' is wrong answer ;(. Correct answer was '{1}'."
+        print(wrong_answer_msg.format(user_answer, answer))
+        print("Let's try again, {0}!".format(user))
         break
     else:  # for - else. Run if no break occurs in cycle for.
-        print('Congratulations, {user}!'.format(user=user))
-
-
-def get_user_answer(question):
-    """Print question and return user answer.
-
-    Args:
-        question: user question
-
-    Returns:
-        user answer
-    """
-    print('Question: {question}'.format(question=question))
-    return prompt.string('Your answer: ', empty=True)
-
-
-def process_wrong_answer(wrong_answer, right_answer):
-    """Print right and wrong answer.
-
-    Args:
-        wrong_answer: wrong answer
-        right_answer: right_answer
-    """
-    print(
-        "'{wrong_answer}' is wrong answer ;(.".format(
-            wrong_answer=wrong_answer,
-        ),
-        end=' ',
-    )
-    print(
-        "Correct answer was '{right_answer}'.".format(
-            right_answer=right_answer,
-        ),
-    )
+        print('Congratulations, {0}!'.format(user))
